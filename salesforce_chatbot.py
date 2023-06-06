cce93f8f4dd505a58b71a650ae8a34f036d245e3
Lars Malmqvist <l.malmqvist@gmail.com>
2023-06-06 14:23:14 +0200

    Changed temparature

--- salesforce_chatbot.py
+++ salesforce_chatbot.py
@@ -34,7 +34,7 @@ def get_index_and_agent():
 
     # set Logging to DEBUG for more detailed outputs
     memory = ConversationBufferMemory(memory_key="chat_history")
-    llm = ChatOpenAI(temperature=0.5)
+    llm = ChatOpenAI(temperature=0)
     agent_executor = initialize_agent(tools, llm, agent="conversational-react-description", memory=memory)
     return index, agent_executor
 
